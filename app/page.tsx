bd5fc73ad27c39029fc68e4a62a192fca0d57338
TenshiFK <124812869+TenshiFK@users.noreply.github.com>
2025-07-23 13:59:21 -0300

    commit cores e fontes configuradas

---
+++ app/page.tsx
@@ -0,0 +1,57 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-secondary font-semibold">
+        Hello world!
+        TESTE
+        MONTSERRAT
+      </h1>
+      <h1 className="font-primary text-4xl">
+        Hello world!
+        TESTE   
+       </h1>
+      <h1 className="text-4xl">
+        Hello world!
+        TESTE   
+       </h1>
+       <p className="text-2xl font-secondary text-gray-primary">
+        Gray Primary
+       </p>
+       <p className="text-2xl font-secondary text-gray-secondary">
+        Gray Secondary
+       </p>
+       <p className="text-2xl font-secondary text-gray-tertiary">
+        Gray Tertiary
+       </p>
+       <p className="text-2xl font-secondary text-gray-quaternary">
+        Gray Quaternary
+       </p>
+       <p className="text-2xl font-secondary text-beige-primary bg-gray-900">
+        Beige Primary
+       </p>
+        <p className="text-2xl font-secondary text-beige-secondary bg-gray-900">
+          Beige Secondary
+        </p>
+        <p className="text-2xl font-secondary text-yellow-primary">
+          Yellow Primary
+        </p>
+        <p className="text-2xl font-secondary text-yellow-secondary">
+          Yellow Secondary  
+        </p>
+        <p className="text-2xl font-secondary text-brown-primary">
+          Brown Primary
+        </p>
+        <p className="text-2xl font-secondary text-brown-secondary">
+          Brown Secondary
+        </p>  
+        <p className="text-2xl font-secondary text-brown-tertiary">
+          Brown Tertiary
+        </p>
+        <p className="text-2xl font-secondary text-green-primary">
+          Green Primary
+        </p>
+
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-primary": "#D9D9D9",
+        "gray-secondary": "#676767",
+        "gray-tertiary": "#666666",
+        "gray-quaternary": "#333333",
+        "beige-primary": "#FAF8F1",
+        "beige-secondary": "#FDF4D1",
+        "yellow-primary": "#E29713",
+        "yellow-secondary": "#FCBA27",
+        "brown-primary": "#5B4116",
+        "brown-secondary": "#5C4418",
+        "brown-tertiary": "#654A1F",
+        "green-primary": "#1FCD00",
+      },
+      fontFamily:{
+          primary: ['"Dela Gothic One"', 'sans-serif'],
+          secondary: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
