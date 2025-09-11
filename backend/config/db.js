dbf706fac05108b0c4f2f9bb340ee90fff562929
Bianca Teixeira <biancadpteixeira@gmail.com>
2025-09-11 13:10:27 -0300

    feat: Adiciona lógica de conexão com DB na nuvem

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,25 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-  user: 'postgres',         
-  host: 'localhost',        
-  database: 'bierbox_db',   
-  password: '784596',
-  port: 5432,        // porta padrão do PostgreSQL
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Se estiver no ambiente de produção (Render), use a DATABASE_URL
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  // Se estiver no ambiente local, use as variáveis do .env
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
