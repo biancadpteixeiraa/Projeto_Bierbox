567999b52910b2b8f8f99e492240960b6c88473e
Bianca Teixeira <biancadpteixeira@gmail.com>
2025-09-18 15:11:19 -0300

    Mudanças

--- backend/controllers/pagamentoController.js
+++ backend/controllers/pagamentoController.js
@@ -69,7 +69,8 @@ exports.criarPreferencia = async (req, res) => {
         };
 
         const result = await preference.create({ body: preferenceBody } );
-        res.status(201).json({ checkoutUrl: result.sandbox_init_point });
+        res.status(201).json({ checkoutUrl: result.init_point });
+
 
     } catch (error) {
         console.error('Erro ao criar preferência de pagamento:', error);
